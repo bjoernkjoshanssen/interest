00c0f45383a8b2bc3a7b04367fd53002c748c9b4
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2026-01-09 11:16:49 -1000

    No more case split r<i

--- Interest.lean
+++ Interest.lean
@@ -1,4 +1,8 @@
 -- This module serves as the root of the `Interest` library.
 -- Import modules here that should be built as part of the library.
 -- import Interest.Basic
+import Interest.Aristotle_CPT_I
+import Interest.Aristotle_duration
+import Interest.AristotleMagic
 import Interest.NFM
+import Interest.NFM_duration
