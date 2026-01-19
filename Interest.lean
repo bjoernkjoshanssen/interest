d0937438ffa18f78d2a8c995aa004fa139c4660b
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2026-01-19 07:17:54 -1000

    Rerun due to 6h timeout

--- Interest.lean
+++ Interest.lean
@@ -1,6 +1,5 @@
 -- This module serves as the root of the `Interest` library.
 -- Import modules here that should be built as part of the library.
--- import Interest.Basic
 import Interest.Aristotle_CPT_I
 import Interest.AristotleMagic
 import Interest.NFM
